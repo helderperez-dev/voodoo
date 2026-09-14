19d05b940b25c55df6a27f11e1302e45face5317
Helder Perez <contact@helderperez.com>
2026-09-14 00:10:17 -0300

    feat(events): export Voodoo Store event bus

--- src/voodoo/storage/events/__init__.py
+++ src/voodoo/storage/events/__init__.py
@@ -1,13 +1,15 @@
-"""Event bus capability (Sprint 7)."""
+"""Event bus capability (Sprint 7, Sprint 28 Store convergence)."""
 
 from voodoo.storage.events.interfaces import EventBusCapabilities, VoodooEventBus
 from voodoo.storage.events.local import LocalEventBus
 from voodoo.storage.events.postgres import PostgresEventStore
 from voodoo.storage.events.sqlite import SQLiteEventBus
+from voodoo.storage.events.store import VoodooStoreEventBus
 
 __all__ = [
     "EventBusCapabilities",
     "VoodooEventBus",
+    "VoodooStoreEventBus",
     "LocalEventBus",
     "PostgresEventStore",
     "SQLiteEventBus",
