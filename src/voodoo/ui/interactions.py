ca7ec774fddbe179eda4374e97ecc0714b4aa30e
Helder Perez <contact@helderperez.com>
2026-09-18 12:16:45 -0300

    fix(ui): restore interactions.py after destructive formatting

--- src/voodoo/ui/interactions.py
+++ src/voodoo/ui/interactions.py
@@ -286,7 +286,8 @@ class Tabs(Component):
             aria_orientation=orientation,
         )
         panels = tuple(
-
+            tab.panel(selected, self.id or "", index)
+            for index, tab in enumerate(tabs)
         )
         self.children = (tablist, *panels)
 
